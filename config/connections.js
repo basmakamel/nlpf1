fb943dbfa33a61cdb74e2c61ef514fd9deea9548
basmakamel <basma.kamel@epita.fr>
2018-12-10 20:59:57 +0100

    [ADD PROJECT]

---
+++ config/connections.js
@@ -0,0 +1,9 @@
+module.exports.connections = {
+  postgres: {
+    adapter: 'sails-postgresql',
+    host: 'localhost',
+    user: 'postgres',
+    password: 'password',
+    database: 'graffitibob'
+  }
+};
