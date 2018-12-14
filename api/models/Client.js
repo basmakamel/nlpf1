d8a53416e54575b4b3062fb0be8a62876132954b
basmakamel <basma.kamel@epita.fr>
2018-12-14 19:44:57 +0100

    [AJOUT FICHIERS] - Ajout intranet et relation base de données

---
+++ api/models/Client.js
@@ -0,0 +1,50 @@
+/**
+ * Client.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    firstname : {
+      type : "string",
+      required: true
+    },
+
+    lastname : {
+      type : "string",
+      required: true
+    },
+
+    email : {
+      type : "string",
+      isEmail: true,
+      required : true
+    },
+
+    phone : {
+      type : "string",
+      required : true
+    }
+
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
--- config/connections.js
+++
@@ -1,9 +0,0 @@
-module.exports.connections = {
-  postgres: {
-    adapter: 'sails-postgresql',
-    host: 'localhost',
-    user: 'postgres',
-    password: 'password',
-    database: 'graffitibob'
-  }
-};
